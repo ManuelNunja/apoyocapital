ea3698fed2fb1a9dbf8ef5154df959aee8ecf77f
ManuelNunja <nunja21@gmail.com>
2022-10-23 03:19:06 -0500

    Correccion control Equipo (ruta)
    Correccion control Equipo (ruta)

--- js/ControlEquipo.js
+++ js/ControlEquipo.js
@@ -14,10 +14,10 @@ class MiembroEquipo{
 
 let textLorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi perferendis necessitatibus laboriosam vitae quas blanditiis qui atque molestias suscipit quo magni dolores, aliquid nemo adipisci. Animi tenetur in nisi eligendi."
 
-let m1 = new MiembroEquipo("m001","../resources/image/equipoapoyo/javierjui.jpg","Javier Jui Azabache","Gerente Comercial","cuenta1@linkedin.com","cuenta1@apoyocapital.com","Director financiero y banquero con 40 años de trayectoria, Scotiabank Perú, Banco Santander, Banco Interamericano de finanzas, Banco Pichincha Perú, con amplia experiencia comercial en el sector bancario, especializado en la introducción de productos financieros, desarrollo de mercados y negocios. Contador Público Bilingüe, con especialización en Finanzas Corporativas y Comercio Exterior")
-let m2 = new MiembroEquipo("m002","../resources/image/equipoapoyo/jorgesarmiento.jpg","Jorge Sarmiento Zorrilla","Gerente Comercial","cuenta2@linkedin.com","cuenta2@apoyocapital.com", textLorem + textLorem)
-let m3 = new MiembroEquipo("m003","../resources/image/equipoapoyo/juanflores.jpg","Juan Flores Lozano","Jefe de Riesgos","cuenta3@linkedin.com","cuenta3@apoyocapital.com", textLorem)
-let m4 = new MiembroEquipo("m004","../resources/image/equipoapoyo/pedropablo.jpg","Pedro Pingo P.","Director","cuenta4@linkedin.com","cuenta4@apoyocapital.com", textLorem + textLorem)
+let m1 = new MiembroEquipo("m001","resources/image/equipoapoyo/javierjui.jpg","Javier Jui Azabache","Gerente Comercial","cuenta1@linkedin.com","cuenta1@apoyocapital.com","Director financiero y banquero con 40 años de trayectoria, Scotiabank Perú, Banco Santander, Banco Interamericano de finanzas, Banco Pichincha Perú, con amplia experiencia comercial en el sector bancario, especializado en la introducción de productos financieros, desarrollo de mercados y negocios. Contador Público Bilingüe, con especialización en Finanzas Corporativas y Comercio Exterior")
+let m2 = new MiembroEquipo("m002","resources/image/equipoapoyo/jorgesarmiento.jpg","Jorge Sarmiento Zorrilla","Gerente Comercial","cuenta2@linkedin.com","cuenta2@apoyocapital.com", textLorem + textLorem)
+let m3 = new MiembroEquipo("m003","resources/image/equipoapoyo/juanflores.jpg","Juan Flores Lozano","Jefe de Riesgos","cuenta3@linkedin.com","cuenta3@apoyocapital.com", textLorem)
+let m4 = new MiembroEquipo("m004","resources/image/equipoapoyo/pedropablo.jpg","Pedro Pingo P.","Director","cuenta4@linkedin.com","cuenta4@apoyocapital.com", textLorem + textLorem)
 
 arrayEquipo.push(m1)
 arrayEquipo.push(m2)
@@ -28,5 +28,3 @@ function BuscarMiembro(codigo){
     let item = arrayEquipo.find(x => x.codigo == codigo)
     return item;
 }
-
-BuscarMiembro("m002")
